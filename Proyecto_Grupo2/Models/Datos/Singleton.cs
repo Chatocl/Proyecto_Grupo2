de786fa8f37599ca90e9c11c8c51e09bb552b763
Christopher-code17 <ncchinchilla@correo.url.edu.gt>
2022-04-30 15:55:47 -0600

    Datos & Árbol AVL

---
+++ Proyecto_Grupo2/Models/Datos/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_Grupo2.Models
+{
+    public class Singleton
+    {
+        private static Singleton _instance = null;
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Singleton();
+                return _instance;
+            }
+        }
+        int bandera;
+        public List<Paciente> aux = new List<Paciente>();
+    }
+}
---
+++ Proyecto_Grupo2/Models/Paciente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Grupo2.Models
+{
+    public class Paciente
+    {
+        [Display(Name = "Nombre completo del paciente")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Nombre { get; set; }
+
+        [Display (Name ="Número de DPI o Partida de nacimiento")]
+        [Required (ErrorMessage ="El campo {0} es requerido.")]
+        public int ID { get; set; }
+        [Display (Name ="Edad del paciente")]
+        [Required(ErrorMessage ="El campo {0} es requerido.")]
+        public int Edad { get; set; }
+        [Display (Name ="Número de telefono")]
+        [Required(ErrorMessage ="El campo {0} es requerido.")]
+        public int Telefono { get; set; }
+        [Display(Name ="Fecha de su última consulta")]
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage ="El campo {0} es requerido.")]
+        public DateTime? FDUltimaConsulta { get; set; }
+        [Display(Name ="Fecha de su próxima consulta(opcional)")]
+        [DataType(DataType.Date)]
+        public DateTime? FDProximaConsulta { get; set; }
+        [Display(Name ="Descripción del ultimo diagnostico o tratamiento que posee(opcional)")]
+        public string Descipion { get; set; }
+    }
+}
