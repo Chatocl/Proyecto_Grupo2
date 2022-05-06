fdcd276adccb3fc271702b5ff13fd8fdb9afa696
MARIA FERNANDA CANESES RUIZ <mfcaneses@correo.url.edu.gt>
2022-05-06 08:00:12 -0600

    View Crear paciente

--- Proyecto_Grupo2/Models/Paciente.cs
+++ Proyecto_Grupo2/Models/Paciente.cs
@@ -14,30 +14,30 @@ namespace Proyecto_Grupo2.Models
         [Required(ErrorMessage = "El campo {0} es requerido.")]
         public string Nombre { get; set; }
 
-        [Display(Name = "Número de DPI o Partida de nacimiento")]
+        [Display(Name = "Número de DPI o partida de nacimiento")]
         [Required(ErrorMessage = "El campo {0} es requerido.")]
-        [StringLength(13, MinimumLength = 13, ErrorMessage = "Tamaño incorrecto del DPI")]
+        [StringLength(13, MinimumLength = 13, ErrorMessage = "Tamaño incorrecto de DPI")]
         public string DPI { get; set; }
 
         [Display(Name = "Edad del paciente")]
         [Required(ErrorMessage = "El campo {0} es requerido.")]
         public string Edad { get; set; }
 
-        [Display(Name = "Número de telefono")]
+        [Display(Name = "Número de teléfono")]
         [Required(ErrorMessage = "El campo {0} es requerido.")]
-        [StringLength(8, MinimumLength = 8, ErrorMessage = "Tamaño incorrecto del DPI")]
+        [StringLength(8, MinimumLength = 8, ErrorMessage = "Tamaño incorrecto de DPI")]
         public string Telefono { get; set; }
         //  [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}", ApplyFormatInEditMode =true)]
-        [Display(Name = "Fecha de su última consulta")]
+        [Display(Name = "Fecha de última consulta")]
         [DataType(DataType.Date)]
         [Required(ErrorMessage = "El campo {0} es requerido.")]
         
         public DateTime? FDU { get; set; }
 
-        [Display(Name ="Fecha de su próxima consulta (opcional)")]
+        [Display(Name ="Fecha de próxima consulta (Opcional)")]
         [DataType(DataType.Date)]
         public DateTime? FDP { get; set; }
-        [Display(Name ="Descripción del ultimo diagnostico o tratamiento que posee (opcional)")]
+        [Display(Name ="Descripción del último diagnóstico o tratamiento que posee (Opcional)")]
         public string Descripcion { get; set; }
 
         public int CompareTo(Paciente otro)
