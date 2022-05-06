b83867bd69d35d86958167bdfc95e887d3900148
CARLOS ROBERTO CORONADO LAZO <crcoronado@correo.url.edu.gt>
2022-05-06 08:21:34 -0600

    Correccion de una eliminacion por merge

--- Proyecto_Grupo2/Models/Datos/Singleton.cs
+++ Proyecto_Grupo2/Models/Datos/Singleton.cs
@@ -23,6 +23,7 @@ namespace Proyecto_Grupo2.Models.Datos
         public int bandera;
         public Paciente AuxP = new Paciente();
         public Clase.Arbol_AVL<Paciente> miAVL = new Clase.Arbol_AVL<Paciente>();
+        public Clase.Lista ListaPacientes = new Clase.Lista();
         // Listas usadas para el seguimiento de los Pacientes 
         public List<Paciente> Aux = new List<Paciente>();
         public List<Paciente> LimDental = new List<Paciente>();
